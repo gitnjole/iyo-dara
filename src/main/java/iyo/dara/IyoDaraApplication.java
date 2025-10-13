ce1bc0827e7b59e6ed26879ff073c76357e2683e
Antonio Cahun <cahunantonio@gmail.com>
2025-10-13 18:59:45 +0200

    init

---
+++ src/main/java/iyo/dara/IyoDaraApplication.java
@@ -0,0 +1,13 @@
+package iyo.dara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IyoDaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IyoDaraApplication.class, args);
+	}
+
+}
