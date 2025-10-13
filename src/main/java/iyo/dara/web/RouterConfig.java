f3feab909f8ab3f63dbb868203087bc3826a525b
Antonio Cahun <cahunantonio@gmail.com>
2025-10-13 22:14:04 +0200

    feat: introduce web layer

---
+++ src/main/java/iyo/dara/web/RouterConfig.java
@@ -0,0 +1,22 @@
+package iyo.dara.web;
+
+import iyo.dara.handler.TransactionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class RouterConfig {
+
+    private static final String PATH_NAME_GET_TRANSACTIONS = "/api/transactions";
+
+    @Bean
+    public RouterFunction<ServerResponse> transactionRoutes(TransactionHandler handler) {
+        return route()
+                .GET(PATH_NAME_GET_TRANSACTIONS, handler::getTransactions)
+                .build();
+    }
+}
