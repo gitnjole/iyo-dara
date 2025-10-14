4d3d4a1cc0deab1475945331daae2e0f78bb735f
Antonio Cahun <antonio.cahun@infoscope.hr>
2025-10-14 10:47:23 +0200

    feat: implement filtering interface

---
+++ src/main/java/iyo/dara/core/domain/DatedAccountable.java
@@ -0,0 +1,10 @@
+package iyo.dara.core.domain;
+
+import iyo.dara.account.domain.AccountType;
+
+import java.time.LocalDate;
+
+public interface DatedAccountable {
+    LocalDate date();
+    AccountType paymentSource();
+}
